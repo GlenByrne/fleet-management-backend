6f968526608eb0d9eadbae7bf8022120f9c559ef
Glen Byrne <glenobyrne123@gmail.com>
2022-02-03 22:37:12 +0000

    Work done for subscriptions

--- src/context.ts
+++ src/context.ts
@@ -1,4 +1,5 @@
 import { FuelCard, PrismaClient } from '@prisma/client';
+import { Request, Response } from 'express';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { PubSub } from 'graphql-subscriptions';
 import { TypedPubSub } from 'typed-graphql-subscriptions';
@@ -13,12 +14,12 @@ const pubSub = new TypedPubSub<PubSubChannels>(new PubSub());
 
 export interface Context {
   prisma: PrismaClient;
-  req: FastifyRequest;
-  res: FastifyReply;
+  req: Request;
+  res: Response;
   pubsSub: typeof pubSub;
 }
 
-export async function contextFactory(req: FastifyRequest, res: FastifyReply) {
+export async function contextFactory(req: Request, res: Response) {
   return {
     prisma,
     req,
--- src/server.ts
+++ src/server.ts
@@ -6,12 +6,19 @@ import {
   getGraphQLParameters,
   processRequest,
   renderGraphiQL,
-  Request,
+  sendMultipartResponseResult,
+  sendResponseResult,
   sendResult,
   shouldRenderGraphiQL,
 } from 'graphql-helix';
 import { envelop, useSchema } from '@envelop/core';
+import * as ws from 'ws';
 import { contextFactory } from 'src/context';
+import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import { GraphQLError, execute, subscribe } from 'graphql';
+import { useServer } from 'graphql-ws/lib/use/ws';
 import { schemaWithPermissions } from './schema';
 
 export const ACCESS_TOKEN_SECRET = 'xudvholxjekszefvsuvosuegv';
@@ -28,62 +35,116 @@ const getEnveloped = envelop({
   plugins: [useSchema(schemaWithPermissions)],
 });
 
-const server = fastify();
+// const server = fastify();
 
-server.register(cookie);
+// server.register(cookie);
 // server.register(fastifyCors);
-server.register(AltairFastify, {
-  path: '/altair',
-  baseURL: '/altair/',
-  endpointURL: '/graphql',
-});
+// server.register(AltairFastify, {
+//   path: '/altair',
+//   baseURL: '/altair/',
+//   endpointURL: '/graphql',
+// });
 
-server.route({
-  method: ['POST', 'GET'],
-  url: '/graphql',
-  handler: async (req, res) => {
-    const { parse, validate, execute, schema } = getEnveloped({
-      req,
+// server.route({
+//   method: ['POST', 'GET'],
+//   url: '/graphql',
+//   handler: async (req, res) => {
+//     const { parse, validate, execute, schema } = getEnveloped({
+//       req,
+//     });
+
+//     const request: Request = {
+//       headers: req.headers,
+//       method: req.method,
+//       query: req.query,
+//       body: req.body,
+//     };
+
+//     if (shouldRenderGraphiQL(request)) {
+//       res.type('text/html');
+//       res.send(
+//         renderGraphiQL({
+//           endpoint: '/graphql',
+//           subscriptionsEndpoint: 'ws://localhost:4000/graphql',
+//         })
+//       );
+//     } else {
+//       const { operationName, query, variables } = getGraphQLParameters(request);
+
+//       const result = await processRequest({
+//         request,
+//         schema,
+//         operationName,
+//         contextFactory: () => contextFactory(req, res),
+//         query,
+//         variables,
+//         parse,
+//         validate,
+//         execute,
+//       });
+
+//       sendResult(result, res.raw);
+//     }
+//   },
+// });
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors());
+
+app.use('/graphql', async (req, res) => {
+  const request = {
+    body: req.body,
+    headers: req.headers,
+    method: req.method,
+    query: req.query,
+  };
+
+  if (shouldRenderGraphiQL(request)) {
+    res.send(
+      renderGraphiQL({
+        subscriptionsEndpoint: 'ws://localhost:4000/graphql',
+      })
+    );
+  } else {
+    const { operationName, query, variables } = getGraphQLParameters(request);
+
+    const result = await processRequest({
+      operationName,
+      query,
+      variables,
+      request,
+      schema: schemaWithPermissions,
+      contextFactory: () => contextFactory(req, res),
     });
 
-    const request: Request = {
-      headers: req.headers,
-      method: req.method,
-      query: req.query,
-      body: req.body,
-    };
-
-    if (shouldRenderGraphiQL(request)) {
-      res.type('text/html');
-      res.send(
-        renderGraphiQL({
-          endpoint: '/graphql',
-          subscriptionsEndpoint: 'ws://localhost:4000/graphql',
-        })
-      );
+    if (result.type === 'RESPONSE') {
+      sendResponseResult(result, res);
+    } else if (result.type === 'MULTIPART_RESPONSE') {
+      sendMultipartResponseResult(result, res);
     } else {
-      const { operationName, query, variables } = getGraphQLParameters(request);
-
-      const result = await processRequest({
-        request,
-        schema,
-        operationName,
-        contextFactory: () => contextFactory(req, res),
-        query,
-        variables,
-        parse,
-        validate,
-        execute,
+      res.status(422);
+      res.json({
+        errors: [
+          new GraphQLError('Subscriptions should be sent over WebSocket.'),
+        ],
       });
-
-      sendResult(result, res.raw);
     }
-  },
+  }
 });
 
 const port = 4000;
 
-server.listen(port, () => {
+const server = app.listen(port, () => {
+  const wsServer = new ws.Server({
+    server,
+    path: '/graphql',
+  });
+
+  useServer({ schema: schemaWithPermissions, execute, subscribe }, wsServer);
+
   console.log(`
       🚀  Server is running!
       🔉  Listening on port 4000
