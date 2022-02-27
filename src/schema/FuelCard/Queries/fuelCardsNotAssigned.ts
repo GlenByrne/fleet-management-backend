cdd294529fe8a686e62d0895cdd0d4f8894c515f
Glen Byrne <glenobyrne123@gmail.com>
2022-02-27 23:08:50 +0000

    Convert fuelCardsNotAssigned back to non-paginated

--- src/schema/FuelCard/Queries/fuelCardsNotAssigned.ts
+++ src/schema/FuelCard/Queries/fuelCardsNotAssigned.ts
@@ -11,77 +11,125 @@ export const FuelCardsNotAssignedInput = inputObjectType({
   },
 });
 
-export const fuelCardsNotAssigned = queryField((t) => {
-  t.connectionField('fuelCardsNotAssigned', {
-    type: FuelCard,
-    nullable: false,
-    additionalArgs: {
-      data: nonNull(
-        arg({
-          type: FuelCardsNotAssignedInput,
-        })
-      ),
-    },
-    resolve: async (_, args, context: Context) => {
-      const userId = verifyAccessToken(context);
+export const fuelCardsNotAssigned = queryField('fuelCardsNotAssigned', {
+  type: list(FuelCard),
+  args: {
+    data: nonNull(
+      arg({
+        type: FuelCardsNotAssignedInput,
+      })
+    ),
+  },
+  resolve: async (_, args, context: Context) => {
+    const userId = verifyAccessToken(context);
 
-      if (!userId) {
-        throw new Error(
-          'Unable to retrieve unassigned fuel cards. You are not logged in.'
-        );
-      }
-
-      const isInOrganisation =
-        await context.prisma.usersOnOrganisations.findUnique({
-          where: {
-            userId_organisationId: {
-              userId,
-              organisationId: args.data.organisationId,
-            },
-          },
-        });
-
-      if (!isInOrganisation) {
-        throw new Error(
-          'Unable to retrieve fuel cards. You are not a member of this organisation'
-        );
-      }
-
-      const offset = args.after ? cursorToOffset(args.after) + 1 : 0;
-
-      if (Number.isNaN(offset)) {
-        throw new Error('Cursor is invalid');
-      }
-
-      const [totalCount, items] = await Promise.all([
-        context.prisma.fuelCard.count({
-          where: {
-            AND: [
-              { vehicleId: null },
-              { organisationId: args.data.organisationId },
-            ],
-          },
-        }),
-        context.prisma.fuelCard.findMany({
-          take: args.first ? args.first : undefined,
-          skip: offset,
-          where: {
-            AND: [
-              { vehicleId: null },
-              { organisationId: args.data.organisationId },
-            ],
-          },
-          orderBy: {
-            cardNumber: 'asc',
-          },
-        }),
-      ]);
-
-      return connectionFromArraySlice(
-        items,
-        { first: args.first, after: args.after },
-        { sliceStart: offset, arrayLength: totalCount }
+    if (!userId) {
+      throw new Error(
+        'Unable to retrieve unassigned fuel cards. You are not logged in.'
       );
-    },
-  });
+    }
+
+    const isInOrganisation =
+      await context.prisma.usersOnOrganisations.findUnique({
+        where: {
+          userId_organisationId: {
+            userId,
+            organisationId: args.data.organisationId,
+          },
+        },
+      });
+
+    if (!isInOrganisation) {
+      throw new Error(
+        'Unable to retrieve fuel cards. You are not a member of this organisation'
+      );
+    }
+
+    return context.prisma.fuelCard.findMany({
+      where: {
+        AND: [
+          { vehicleId: null },
+          { organisationId: args.data.organisationId },
+        ],
+      },
+      orderBy: {
+        cardNumber: 'asc',
+      },
+    });
+  },
 });
+
+// export const fuelCardsNotAssigned = queryField((t) => {
+//   t.connectionField('fuelCardsNotAssigned', {
+//     type: FuelCard,
+//     nullable: false,
+//     additionalArgs: {
+//       data: nonNull(
+//         arg({
+//           type: FuelCardsNotAssignedInput,
+//         })
+//       ),
+//     },
+//     resolve: async (_, args, context: Context) => {
+//       const userId = verifyAccessToken(context);
+
+//       if (!userId) {
+//         throw new Error(
+//           'Unable to retrieve unassigned fuel cards. You are not logged in.'
+//         );
+//       }
+
+//       const isInOrganisation =
+//         await context.prisma.usersOnOrganisations.findUnique({
+//           where: {
+//             userId_organisationId: {
+//               userId,
+//               organisationId: args.data.organisationId,
+//             },
+//           },
+//         });
+
+//       if (!isInOrganisation) {
+//         throw new Error(
+//           'Unable to retrieve fuel cards. You are not a member of this organisation'
+//         );
+//       }
+
+//       const offset = args.after ? cursorToOffset(args.after) + 1 : 0;
+
+//       if (Number.isNaN(offset)) {
+//         throw new Error('Cursor is invalid');
+//       }
+
+//       const [totalCount, items] = await Promise.all([
+//         context.prisma.fuelCard.count({
+//           where: {
+//             AND: [
+//               { vehicleId: null },
+//               { organisationId: args.data.organisationId },
+//             ],
+//           },
+//         }),
+//         context.prisma.fuelCard.findMany({
+//           take: args.first ? args.first : undefined,
+//           skip: offset,
+//           where: {
+//             AND: [
+//               { vehicleId: null },
+//               { organisationId: args.data.organisationId },
+//             ],
+//           },
+//           orderBy: {
+//             cardNumber: 'asc',
+//           },
+//         }),
+//       ]);
+
+//       return connectionFromArraySlice(
+//         items,
+//         { first: args.first, after: args.after },
+//         { sliceStart: offset, arrayLength: totalCount }
+//       );
+//     },
+//   });
+// });
